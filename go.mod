589536af3693193b923f300c4381a347b05545b5
Geof <46729592+geofduf@users.noreply.github.com>
2020-04-13 17:17:33 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/geofduf/logging
+
+go 1.14
---
+++ logging.go
@@ -0,0 +1,108 @@
+package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+const (
+	FATAL = iota
+	SYSTEM
+	ERROR
+	WARNING
+	INFO
+	DEBUG
+	defaultLevel int = WARNING
+)
+
+var logLevels [6]string = [6]string{"FTL", "SYS", "ERR", "WNG", "INF", "DBG"}
+
+var logger *Logger
+
+func GetLogger() *Logger {
+	return logger
+}
+
+type Logger struct {
+	sync.RWMutex
+	level int
+}
+
+func (l *Logger) FATAL(source string, messages ...string) {
+	l.write(FATAL, source, messages)
+}
+
+func (l *Logger) SYSTEM(source string, messages ...string) {
+	l.write(SYSTEM, source, messages)
+}
+
+func (l *Logger) ERROR(source string, messages ...string) {
+	l.write(ERROR, source, messages)
+}
+
+func (l *Logger) WARNING(source string, messages ...string) {
+	l.write(WARNING, source, messages)
+}
+
+func (l *Logger) INFO(source string, messages ...string) {
+	l.write(INFO, source, messages)
+}
+
+func (l *Logger) DEBUG(source string, messages ...string) {
+	l.write(DEBUG, source, messages)
+}
+
+func (l *Logger) SetLevel(level int) {
+	if level < 1 || level >= len(logLevels) {
+		l.SYSTEM("LOG", fmt.Sprintf("cannot set log level to %d", level))
+	} else {
+		l.Lock()
+		l.level = level
+		l.Unlock()
+		l.SYSTEM("LOG", fmt.Sprintf("setting log level to %d (%s)", level, logLevels[level]))
+	}
+}
+
+func (l *Logger) GetLevel() int {
+	l.RLock()
+	defer l.RUnlock()
+	return l.level
+}
+
+func (l *Logger) write(level int, source string, messages []string) {
+	currentLevel := l.GetLevel()
+	if currentLevel >= level {
+		for _, message := range messages {
+			if level == FATAL {
+				log.Fatalf("[%s] [%s] %s\n", logLevels[level], source, message)
+			} else {
+				log.Printf("[%s] [%s] %s\n", logLevels[level], source, message)
+			}
+		}
+	}
+}
+
+func (l *Logger) signalHandler() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGUSR1, syscall.SIGUSR2)
+	for {
+		s := <-c
+		currentLevel := l.GetLevel()
+		switch s {
+		case syscall.SIGUSR1:
+			l.SetLevel(currentLevel + 1)
+		case syscall.SIGUSR2:
+			l.SetLevel(currentLevel - 1)
+		}
+	}
+}
+
+func init() {
+	logger = new(Logger)
+	logger.level = defaultLevel
+	go logger.signalHandler()
+}
