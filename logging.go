ec3597031b73e8d38eb1d203865ff5b1b7bc0d61
Geof <46729592+geofduf@users.noreply.github.com>
2022-09-01 21:40:52 +0200

    remove package level logger

--- logging.go
+++ logging.go
@@ -21,63 +21,61 @@ const (
 
 var logLevels [6]string = [6]string{"FTL", "SYS", "ERR", "WNG", "INF", "DBG"}
 
-var logger *Logger
-
-func GetLogger() *Logger {
-	return logger
-}
-
-type Logger struct {
-	sync.RWMutex
-	level int
+type logger struct {
+	mu     sync.RWMutex
+	level  int
 	listen bool
 }
 
-func (l *Logger) GetLevel() int {
-	l.RLock()
-	defer l.RUnlock()
+func New() *logger {
+	return &logger{level: defaultLevel}
+}
+
+func (l *logger) GetLevel() int {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
 	return l.level
 }
 
-func (l *Logger) SetLevel(level int) {
+func (l *logger) SetLevel(level int) {
 	if level < 1 || level >= len(logLevels) {
 		l.System("LOG", fmt.Sprintf("cannot set log level to %d", level))
 	} else {
-		l.Lock()
+		l.mu.Lock()
 		l.level = level
-		l.Unlock()
+		l.mu.Unlock()
 		l.System("LOG", fmt.Sprintf("setting log level to %d (%s)", level, logLevels[level]))
 	}
 }
 
-func (l *Logger) Fatal(source string, messages ...string) {
+func (l *logger) Fatal(source string, messages ...string) {
 	if len(messages) > 1 {
 		l.write(Fatal, source, messages[:len(messages)-1])
 	}
 	log.Fatalf("[%s] [%s] %s\n", logLevels[Fatal], source, messages[len(messages)-1])
 }
 
-func (l *Logger) System(source string, messages ...string) {
+func (l *logger) System(source string, messages ...string) {
 	l.write(System, source, messages)
 }
 
-func (l *Logger) Error(source string, messages ...string) {
+func (l *logger) Error(source string, messages ...string) {
 	l.write(Error, source, messages)
 }
 
-func (l *Logger) Warning(source string, messages ...string) {
+func (l *logger) Warning(source string, messages ...string) {
 	l.write(Warning, source, messages)
 }
 
-func (l *Logger) Info(source string, messages ...string) {
+func (l *logger) Info(source string, messages ...string) {
 	l.write(Info, source, messages)
 }
 
-func (l *Logger) Debug(source string, messages ...string) {
+func (l *logger) Debug(source string, messages ...string) {
 	l.write(Debug, source, messages)
 }
 
-func (l *Logger) write(level int, source string, messages []string) {
+func (l *logger) write(level int, source string, messages []string) {
 	if level <= System || l.GetLevel() >= level {
 		for _, message := range messages {
 			log.Printf("[%s] [%s] %s\n", logLevels[level], source, message)
@@ -85,9 +83,9 @@ func (l *Logger) write(level int, source string, messages []string) {
 	}
 }
 
-func (l *Logger) ListenForSignal() {
+func (l *logger) ListenForSignal() {
 	var message string
-	l.Lock()
+	l.mu.Lock()
 	if l.listen {
 		message = "cannot register signal handler more than once"
 	} else {
@@ -95,11 +93,11 @@ func (l *Logger) ListenForSignal() {
 		l.listen = true
 		message = "registering signal handler"
 	}
-	l.Unlock()
+	l.mu.Unlock()
 	l.System("LOG", message)
 }
 
-func (l *Logger) signalHandler() {
+func (l *logger) signalHandler() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGUSR1, syscall.SIGUSR2)
 	for {
@@ -112,10 +110,4 @@ func (l *Logger) signalHandler() {
 			l.SetLevel(currentLevel - 1)
 		}
 	}
-}
-
-func init() {
-	logger = new(Logger)
-	logger.level = defaultLevel
-	logger.listen = false
 }
